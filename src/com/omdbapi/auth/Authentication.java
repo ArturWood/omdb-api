77499ed3e32397a3fb64458c49c4afe0dbb26d17
unknown <arturilenwood@gmail.com>
2023-06-03 00:12:37 -0300

    Correção do caminho para arquivo config

--- src/com/omdbapi/auth/Authentication.java
+++ src/com/omdbapi/auth/Authentication.java
@@ -9,7 +9,7 @@ public class Authentication {
         Properties properties = new Properties();
 
         try {
-            FileInputStream file = new FileInputStream("config.properties");
+            FileInputStream file = new FileInputStream("src/config.properties");
             properties.load(file);
             file.close();
         } catch (IOException e) {
