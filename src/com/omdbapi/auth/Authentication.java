35a15261fbd81f186fcfb7f60dede8edbf556b47
unknown <arturilenwood@gmail.com>
2023-06-02 22:44:08 -0300

    Adição da classe reponsavel por inserir a autenticação no request para a API, adição de um arquivo config.example para demonstrar como inserir uma autenticação própria e a atualização do arquivo gitignore para remover o arquivo config.properties com a chave de acesso verdadeira do GIT

--- src/com/omdbapi/auth/Authentication.java
+++ src/com/omdbapi/auth/Authentication.java
@@ -1,2 +1,27 @@
-package com.omdbapi.auth;public class Authentication {
+package com.omdbapi.auth;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Authentication {
+    public String getAuth() {
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream file = new FileInputStream("config.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            e.getMessage();
+            return null;
+        }
+
+        String apiKey = properties.getProperty("api.key");
+        if (apiKey == null) {
+            System.err.println("Chave de segurança da API não encontrada no arquivo de configurações.");
+            return null;
+        }
+        return apiKey;
+    }
 }
